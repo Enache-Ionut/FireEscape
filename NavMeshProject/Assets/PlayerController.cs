7eddf610c65f3cff2078bb0732b050efeaa04eb4
Enache Ionut <enachestoianionut@gmail.com>
2019-02-26 16:24:20 +0200

    import nav mesh project

---
+++ NavMeshProject/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+  //public Camera cam;
+  public NavMeshAgent agent;
+
+  // Update is called once per frame
+  private void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    //  Debug.Log("Update!!\n");
+      RaycastHit hit;
+
+      if (Physics.Raycast(ray, out hit))
+      {
+        NavMeshHit navmeshHit;
+
+        int walkableMask = 1 << NavMesh.GetAreaFromName("Walkable");
+
+        if (NavMesh.SamplePosition(hit.point, out navmeshHit, 1.0f, walkableMask))
+        {
+          agent.SetDestination(navmeshHit.position);
+        }
+
+        // move the agent
+        //agent.SetDestination(hit.point);
+
+      }
+    }
+  }
+}

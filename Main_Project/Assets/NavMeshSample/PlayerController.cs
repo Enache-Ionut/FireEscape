7d36e36aa53469c197f97c8999597aed878d86e2
Enache Ionut <enachestoianionut@gmail.com>
2019-02-28 19:11:35 +0200

    find the closest exit

--- Main_Project/Assets/NavMeshSample/PlayerController.cs
+++ Main_Project/Assets/NavMeshSample/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
@@ -10,16 +11,53 @@ public class PlayerController : MonoBehaviour
   public Transform spawneePosition;
   public ThirdPersonCharacter character;
 
+  private int waitCounter = 0;
+
+  private GameObject[] exits;
+  private Transform closestGameObjectTransform;
 
   void Start()
   {
     agent.updateRotation = false;
+    exits = GameObject.FindGameObjectsWithTag("Exit");
+  }
+
+
+  Transform GetClosestExit(Transform[] enemies)
+  {
+    Transform tMin = null;
+    float minDist = Mathf.Infinity;
+    Vector3 currentPos = transform.position;
+    foreach (Transform t in enemies)
+    {
+      float dist = Vector3.Distance(t.position, currentPos);
+      if (dist < minDist)
+      {
+        tMin = t;
+        minDist = dist;
+      }
+    }
+    return tMin;
   }
 
 
   // Update is called once per frame
   private void Update()
   {
+
+    if(waitCounter < 100)
+    {
+      ++waitCounter;
+      return;
+    }
+
+
+    //Transform[] exitsTrasnforms = exits.Select(exit => exit.GetComponent<Transform>()).ToArray();
+    //closestGameObjectTransform = GetClosestExit(exitsTrasnforms);
+
+
+
+
     if (Input.GetMouseButtonDown(0))
     {
       Ray ray = cam.ScreenPointToRay(Input.mousePosition);

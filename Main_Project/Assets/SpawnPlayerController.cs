5031bb69d8cd7e917f683382cc41188153c3235d
Enache Ionut <enachestoianionut@gmail.com>
2019-02-28 20:21:19 +0200

    populate the map with players

---
+++ Main_Project/Assets/SpawnPlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class SpawnPlayerController : MonoBehaviour
+{
+  private GameObject[] positions;
+  public PlayerController player;
+
+  // Start is called before the first frame update
+  private void Start()
+  {
+    var playersLocations = GameObject.FindGameObjectsWithTag("PlayerSpawnLocation").Select(obj => obj.transform);
+    foreach(var location in playersLocations)
+    {
+      Instantiate(player, location.position, location.rotation);
+    }
+  }
+
+  // Update is called once per frame
+  private void Update()
+  {
+
+  }
+}

ccb6690a98ed001b0fc9e2c9b2b6cf907b69db96
Albastroiu Radu <albastroiuradug@gmail.com>
2019-02-28 06:05:19 -0800

    Added random spawn locations.

--- Main_Project/Assets/Effects Behaviour/EffectsController.cs
+++ Main_Project/Assets/Effects Behaviour/EffectsController.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class EffectsController : MonoBehaviour
 {
     [SerializeField] GameObject[] effects;
+    Transform[] spawnLocations;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject[] spawnLocationsGameObj = GameObject.FindGameObjectsWithTag("SpawnLocation");
+        spawnLocations = new Transform[spawnLocationsGameObj.Length];
+        int count = 0;
+        foreach (var spawnLocation in spawnLocationsGameObj)
+        {
+            spawnLocations[count] = spawnLocation.transform;
+            count++;
+        }
+
+        SpawnRandomFires();
+    }
+
+    void SpawnRandomFires()
+    {
+        System.Random rnd = new System.Random();
+        int rndnumber = rnd.Next(8, spawnLocations.Length);
+        for (int i = 0; i < rndnumber; i++)
+        {
+            int rndindex = rnd.Next(0, spawnLocations.Length);
+            Instantiate(effects[rnd.Next(0, 2)], spawnLocations[rndindex]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
